b55457b7a280c2e09f08f5a3b932a2e1ae561dcd
Wajid216 <ali.wajid216@gmail.com>
2017-12-12 16:40:45 +0500

    assgniment

---
+++ WebApplication9/Controllers/BatchController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication9.Controllers
+{
+    public class BatchController : ApiController
+    {
+
+        MiniAttendenceSystemEntities db = new MiniAttendenceSystemEntities();
+
+
+        [Route("api/GetBatches")]
+        public List<Batch> GetBatches()
+        {
+
+            db.Configuration.ProxyCreationEnabled = false;
+
+            return db.Batches.ToList();
+
+        }
+
+        [Route("api/GetStudents/{batch}")]
+        public List<Student> GetStudents(string batch)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+
+            return db.Students.Where(u => u.Batch.Name == batch).ToList();
+        
+        }
+       
+
+
+
+    }
+}
